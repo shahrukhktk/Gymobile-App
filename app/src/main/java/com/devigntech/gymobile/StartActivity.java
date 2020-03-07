d921cce1c3426d3fad35e423eda45390e09f9247
shahr <shahrukhktk760@gmail.com>
2020-03-07 13:02:48 +0500

    Start Screen

--- app/src/main/java/com/devigntech/gymobile/StartActivity.java
+++ app/src/main/java/com/devigntech/gymobile/StartActivity.java
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
+
 public class StartActivity extends AppCompatActivity
 {
+    private SwipeButton trainer, client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        trainer = (SwipeButton) findViewById(R.id.swipe_as_trainer_ID);
+        client = (SwipeButton) findViewById(R.id.swipe_as_client_ID);
+
+        trainer.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+
+            }
+        });
+
+        client.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+
+            }
+        });
+
     }
 }
