fc46605b59eaa0080f4edf6fd5d641001bed6c90
shahr <shahrukhktk760@gmail.com>
2020-03-07 15:46:08 +0500

    Sign In - Trainer

---
+++ app/src/main/java/com/devigntech/gymobile/TrainerAppFiles/Trainer_Authentication/Trainer_Login.java
@@ -0,0 +1,17 @@
+package com.devigntech.gymobile.TrainerAppFiles.Trainer_Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devigntech.gymobile.R;
+
+public class Trainer_Login extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer__login);
+    }
+}
