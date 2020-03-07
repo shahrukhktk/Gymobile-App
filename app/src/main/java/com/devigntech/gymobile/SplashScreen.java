116173de46a419b50a7dee8d3cc859baead5d665
shahr <shahrukhktk760@gmail.com>
2020-03-07 11:50:06 +0500

    SplashScreen

--- app/src/main/java/com/devigntech/gymobile/SplashScreen.java
+++ app/src/main/java/com/devigntech/gymobile/SplashScreen.java
@@ -3,14 +3,23 @@ package com.devigntech.gymobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity
 {
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //TO REMOVE THE NOTIFICATION BAR FROM SPLASH SCREEN
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.splash_progress_ID);
     }
 }
